3f3a8ba07ab401b7402e66e1f2d2c4a1131c4c68
Fully34 <cfullinw34@gmail.com>
2015-07-01 11:40:29 -0600

    done with exercise

--- script.js
+++ script.js
@@ -59,3 +59,39 @@ function nameAndCity(arr) {
     })
 }
 
+// -> Victory
+
+//===========================================================================//
+                        /* ~~~ # 3 ~~~ */ 
+//===========================================================================//
+
+function writeSentence(arr) {
+
+    // loop thru array elements, print object.age
+    arr.map(function(obj){
+
+        console.log(obj.name + " is from " + obj.city);
+    })
+}
+
+// -> Victory
+
+
+//===========================================================================//
+                        /* ~~~ # 4 ~~~ */ 
+//===========================================================================//
+
+function olderThan25(arr) {
+
+    // loop thru array objects
+    arr.map(function(obj){
+
+        // if object.age > 25 --> print obj.name + " is older than 25"
+        if (obj.age > 25) {
+
+            console.log( obj.name + " is older than 25" );
+        }
+    })
+}
+
+// -> Victory
